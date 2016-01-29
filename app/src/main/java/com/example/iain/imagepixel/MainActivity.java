07b3386fdf4980111208aa3dcd33172766d738f2
Iain Letourneau <iain.letourneau@gmail.com>
2016-01-29 16:06:28 +0800

    Added Bluetooth to device

--- app/src/main/java/com/example/iain/imagepixel/MainActivity.java
+++ app/src/main/java/com/example/iain/imagepixel/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.iain.imagepixel;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -7,14 +11,20 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +32,23 @@ public class MainActivity extends AppCompatActivity {
     String imgDecodableString;
     String[][] blueSend = new String[32][16];
 
-    // Declare the button b1 and ImageView im
+    // Declare things
     Button b1;
     ImageView im;
 
     // Declare the bitmaps bmp and operation
     private Bitmap bmp;
     private Bitmap operation;
+
+    // BLUETOOTH SECTION
+    Button btnSend, btnConnect;
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    // SPP UUID. Look for it
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +61,69 @@ public class MainActivity extends AppCompatActivity {
 
         BitmapDrawable abmp = (BitmapDrawable) im.getDrawable();
         bmp = Bitmap.createScaledBitmap(abmp.getBitmap(), 384, 192, true);
+
+        Intent newint = getIntent();
+        address = newint.getStringExtra(DeviceList.EXTRA_ADDRESS);
+
+        btnConnect = (Button) findViewById(R.id.btn_bluetooth);
+        btnSend = (Button) findViewById(R.id.btn_send);
+
+        new ConnectBT().execute();
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //connect to devices
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Send to device
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void Disconnect() {
+        if (btSocket!= null) {
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+        finish();
+    }
+
+    private void sendImage(){
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("TO".toString().getBytes());
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
     }
 
     public void loadImagefromGallery(View view) {
@@ -134,4 +217,41 @@ public class MainActivity extends AppCompatActivity {
         Log.v("TESTING", "SEND TEST: " + blueSend[0][0] + "~~" + blueSend[0][1]);
     }
 
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(MainActivity.this, "Connecting...", "Please wait!!!");
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+            try {
+                if (btSocket == null || !isBtConnected) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();
+                }
+            } catch (IOException e) {
+                ConnectSuccess = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if(!ConnectSuccess) {
+                msg("Connection Faled. Is it a SPP Bluetooth? Try again.");
+                finish();
+            } else {
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
 }
